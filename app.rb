6483908bc1d38615176031d89595a99d710149f2
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-12-22 01:21:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,5 +6,10 @@ class App < Sinatra::Base
     @reverse_name = params[:name].reverse
     @reverse_name
   end
+  
+  get '/reversename/:name' do
+    @reverse_name = params[:name].reverse
+    @reverse_name
+  end
 
 end
