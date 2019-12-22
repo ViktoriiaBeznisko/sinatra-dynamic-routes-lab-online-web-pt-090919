4e7b0d7b8ea87612f45efe49fe6ee4df97882409
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-12-22 01:39:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,8 +19,6 @@ class App < Sinatra::Base
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @num = params[:number].to_i
-    @phr = params[:phrase]
-    @phr * @num
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 end
